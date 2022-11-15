7d8ab6c4622115ad8341b3641a400e2b2dea38d9
inna9Z <vozniakinna@gmail.com>
2022-11-15 23:10:55 +0100

    fix lint check issue

--- src/deep-flat/deep-flat.js
+++ src/deep-flat/deep-flat.js
@@ -3,13 +3,13 @@
 @param {Array[]} [array = []] // Converts a nested array into a single array with no nesting
 @returns {Array} // It returns a new array and there are no side-effects
 @example 
- deepFlat([1, [2, [3, [4]], 5]]);
- -> [1, 2, 3, 4, 5]
- deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
- -> ['a', 'b', 'c', 'd', 'e']
-*/
+    deepFlat([1, [2, [3, [4]], 5]]);
+    -> [1, 2, 3, 4, 5]
+    deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
+    -> ['a', 'b', 'c', 'd', 'e']
+    */
 
 export const deepFlat = (array = []) => {
-  const newArray = [...array];
-  return newArray.flat(Infinity);
+    const newArray = [...array];
+    return newArray.flat(Infinity);
 };
