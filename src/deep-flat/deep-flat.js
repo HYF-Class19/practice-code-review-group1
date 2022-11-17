f668274bdf55d1de648a006803ab2edd4ef8614e
inna9Z <vozniakinna@gmail.com>
2022-11-17 17:59:36 +0100

    fixed lint error

--- src/deep-flat/deep-flat.js
+++ src/deep-flat/deep-flat.js
@@ -1,15 +1,24 @@
-/** 
-
-@param {Array[]} [array = []] // Converts a nested array into a single array with no nesting
-@returns {Array} // It returns a new array and there are no side-effects
-@example 
-    deepFlat([1, [2, [3, [4]], 5]]);
-    -> [1, 2, 3, 4, 5]
-    deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
-    -> ['a', 'b', 'c', 'd', 'e']
-    */
+/**
+ * Converts a nested array into a single array with no nesting.
+ *
+ * It returns a new array and there are no side-effects.
+ *
+ * @param {Array} [array=[]] - The array to flatten.
+ * @returns {Array} Returns the new flattened array.
+ * @example
+ *
+ * deepFlat([1, [2, [3, [4]], 5]]);
+ * // -> [1, 2, 3, 4, 5]
+ *
+ * @example
+ *
+ * deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
+ * // -> ['a', 'b', 'c', 'd', 'e']
+ */
 
 export const deepFlat = (array = []) => {
+  // copied our initial array to avoid any side effects
   const newArray = [...array];
+  // the return function will show new flattened array
   return newArray.flat(Infinity);
 };
--- src/deep-flat/deep-flat.spec.js
+++ src/deep-flat/deep-flat.spec.js
@@ -1,30 +1,39 @@
 import { deepFlat } from './deep-flat.js';
 
 describe('Converts a nested array into a single array with no nesting', () => {
-  describe('Converts a nested array into a single array', () => {
-    it('whole numbers', () => {
-      const expected = [1, 2, 3, 4, 5];
-      const actual = deepFlat([1, [2, [3, [4]], 5]]);
-      expect(actual).toEqual(expected);
-    });
-    it('whole letters', () => {
-      const expected = ['a', 'b', 'c', 'd', 'e'];
-      const actual = deepFlat(['a', ['b'], [['c'], ['d']], ['e']]);
-      expect(actual).toEqual(expected);
-    });
-    it('for numbers and leters', () => {
-      const expected = [1, 2, 3, 'a', 'b', 'c', 'd'];
-      const actual = deepFlat([1, [2], [3], ['a'], [['b']], ['c'], [['d']]]);
-      expect(actual).toEqual(expected);
-    });
-    it('with empty array', () => {
-      const expected = [];
-      const actual = deepFlat([]);
-      expect(actual).toEqual(expected);
-    });
-    it('the function should return new array', () => {
-      const array = [1, [2, [3, [4]], 5]];
-      expect(deepFlat(array) === array).toEqual(false);
-    });
+  it('array with numbers ', () => {
+    const expected = [1, 2, 3, 4, 5];
+    const received = deepFlat([1, [2, [3, [4]], 5]]);
+    expect(received).toEqual(expected);
+  });
+  it('array with words ', () => {
+    const expected = ['h', 'a', 'p', 'p', 'y'];
+    const received = deepFlat([['h', ['a', ['p']], ['p'], 'y']]);
+    expect(received).toEqual(expected);
+  });
+  it('when array is string', () => {
+    const expected = ['', ''];
+    const received = deepFlat([[''], ['']]);
+    expect(received).toEqual(expected);
+  });
+});
+describe('Has not side effect', () => {
+  it('The function should return new array', () => {
+    const array = [1, [2, [3, [4]], 5]];
+    expect(deepFlat(array) === array).toEqual(false);
+  });
+});
+describe('When empty array passed function should not throw an error ', () => {
+  it('When is epty array', () => {
+    const expected = [];
+    const received = deepFlat([]);
+    expect(expected).toEqual(received);
+  });
+});
+describe('when no argument passed should apply default argument', () => {
+  it('When default argument', () => {
+    const expected = [];
+    const received = deepFlat();
+    expect(received).toEqual(expected);
   });
 });
